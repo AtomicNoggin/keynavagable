0d236d6a16c338a004ada9b8c17f98ac25fce9f7
Patrick Denny <heythere@atomicnoggin.ca>
2019-07-10 14:25:37 -0400

    bug fixes & code cleanup

--- README.md
+++ README.md
@@ -1,20 +1,22 @@
 # keynavagable
+
 Lightweight keyboard navigation manager. Extends the Element object to allow
 custom key navigation events to be set on focusable Elements, or capture
 key navigation events of child focusable Elements.
 
 ## Usage
+
 ```js
 // assumes list Elements are focusable (e.g. tabindex="-1")
-var listItems = document.querySelectorAll('[role="list"] > [role="listitem"]');
+let listItems = document.querySelectorAll('[role="list"] > [role="listitem"]');
 [].forEach.call(listItems, function(item) {
   // set focus to appropriate sibling with arrow keys
-  item.setKeyAction('DownArrow',function() { item.nextElementSibling.focus() });
-  item.setKeyAction('UpArrow',function() { item.previousElementSibling.focus() });
+  item.setKeyAction('ArrowDown',function() { item.nextElementSibling.focus() });
+  item.setKeyAction('ArrowUp',function() { item.previousElementSibling.focus() });
 });
 
 // list does not need to be focusable to capture a key action
-var lists = document.querySelectorAll('[role="list"]');
+let lists = document.querySelectorAll('[role="list"]');
 [].forEach.call(lists, function(list) {
   // if child element has focus move to top or bottom of list on Home/End
   list.captureKeyAction('Home',function() { list.firstElementChild.focus(); });
@@ -23,33 +25,43 @@ var lists = document.querySelectorAll('[role="list"]');
 ```
 
 ## Properties
+
 ### Element.prototype.tabNext
+
 Set the element to focus on when tab is pressed.
 Value can either be an Element or a string representing the id of an Element. Will also read the id value set in a `tab-next` attribute.
 
 ### Element.prototype.tabPrevious
+
 Set the element to focus on when shift-tab is pressed.
 Value can either be an Element or a string representing the id of an Element. Will also read the id value set in a `tab-previous` attribute.
 
 ## Methods
-### Element.prototype.setKeyAction(key,method[,target])
-#### Arguments:
-**key**: the key press or key press combination to listen for while this element has focus. Maps to the KeyboardEvent key value (with the exception of ' ', which becomes 'Space').
-   If modifier keys are required, those should added prior to the key (e.g. `Shift UpArrow` or `Alt Control PageDown`) in alphabetical order
 
-   *Note*: for alpha-numeric keys, Shift does not need to be provided, as the key value will already detail this
-   (e.g. `Shift w` will not work, but `W` will)
+### Element.prototype.setKeyAction(key,method[,target])
+
+#### Arguments:
+
+**key**: the key press or key press combination to listen for while this element has focus. Maps to the KeyboardEvent key value (with the exception of ' ', which becomes 'Space').
+If modifier keys are required, those should added prior to the key (e.g. `Shift UpArrow` or `Alt Control PageDown`) in alphabetical order
+
+_Note_: for alpha-numeric keys, Shift does not need to be provided, as the key value will already detail this
+(e.g. `Shift w` will not work, but `W` will)
 
 **method**: the function to fire when this key combination is pressed. Can be an actual function, or a string that represents the name of the Element's method to fire.
 
 **target**: the Element to bind to the method. Defaults to the current Element if omitted.
 
 ### Element.prototype.removeKeyAction(key)
+
 #### Arguments:
+
 **key**: the key press or key press combination to stop listening for.
 
 ### Element.prototype.captureKeyAction(key,method[,target])
+
 #### Arguments:
+
 **key**: the key press or key press combination to listen for while children of this element has focus.
 
 **method**: the function to fire when this key combination is pressed. Can be an actual function, or a string that represents the name of the Element's method to fire.
@@ -57,5 +69,7 @@ Value can either be an Element or a string representing the id of an Element. Wi
 **target**: the Element to bind to the method. Defaults to the current Element if omitted.
 
 ### Element.prototype.releaseKeyAction(key)
+
 #### Arguments:
+
 **key**: the key press or key press combination to stop listening for on behalf of this elements children
--- keynavagable.js
+++ keynavagable.js
@@ -1,170 +1,184 @@
-!function() {
+!(function() {
   //assumes WeakMap is polyfilled
   var events = new WeakMap();
   var captures = new WeakMap();
   var tabElements = new WeakMap();
   var captureKeys = {};
   var ep = Element.prototype;
-  var next = 'next',tabnext = 'tab-next',prev = 'prev',tabprev = 'tab-previous';
+  var next = "next",
+    tabnext = "tab-next",
+    prev = "prev",
+    tabprev = "tab-previous";
   Object.defineProperties(ep, {
-    'tabNext':{
+    tabNext: {
       get: function() {
         var tabs = tabElements.get(this);
         if (tabs && tabs[next]) {
           return tabs[next];
-        }
-        else {
+        } else {
           return document.getElementById(this.getAttribute(tabnext));
         }
       },
-      set:function (value) {
+      set: function(value) {
         var tabs = tabElements.get(this) || {};
         if (value instanceof Element) {
           tabs[next] = value;
           this.removeAttribute(tabnext);
-        }
-        else if (value=== String(value) && value.length) {
+        } else if (value === String(value) && value.length) {
           tabs[next] = null;
-          this.setAttribute(tabnext,value);
-        }
-        else {
+          this.setAttribute(tabnext, value);
+        } else {
           tabs[next] = null;
           this.removeAttribute(tabnext);
         }
-        tabElements.set(this,tabs);
+        tabElements.set(this, tabs);
       },
-      enumerable:true,
-      configurable:true
+      enumerable: true,
+      configurable: true
     },
-    'tabPrevious':{
+    tabPrevious: {
       get: function() {
         var tabs = tabElements.get(this);
         if (tabs && tabs[prev]) {
           return tabs[prev];
-        }
-        else {
+        } else {
           return document.getElementById(this.getAttribute(tabprev));
         }
       },
-      set:function (value) {
+      set: function(value) {
         var tabs = tabElements.get(this) || {};
         if (value instanceof Element) {
           tabs[prev] = value;
           this.removeAttribute(tabprev);
-        }
-        else if (value=== String(value) && value.length) {
+        } else if (value === String(value) && value.length) {
           tabs[prev] = null;
-          this.setAttribute(tabprev,value);
-        }
-        else {
+          this.setAttribute(tabprev, value);
+        } else {
           tabs[prev] = null;
           this.removeAttribute(tabprev);
         }
         tabElements.set(this, tabs);
       },
-      enumerable:true,
-      configurable:true
+      enumerable: true,
+      configurable: true
     }
   });
-  ep.setKeyAction = function setKeyAction(key,method,element) {
-      var myEvents = events.get(this) || {};
-      element = element instanceof Element ? element : this;
-      //if method is a string matching a method of the element, use that.
-      if (method && method === ''+method && element[method] instanceof Function) {
-        method = element[method];
-      }
-      //if it's not a function, use a dummy.
-      else if (!(typeof method === 'function')) {
-        method = function() {};
-      }
-      if (method) {
-        myEvents[key] = method.bind(element);
-        events.set(this,myEvents);
-      }
+  ep.setKeyAction = function setKeyAction(key, method, element) {
+    var myEvents = events.get(this) || {};
+    element = element instanceof Element ? element : this;
+    //if method is a string matching a method of the element, use that.
+    if (
+      method &&
+      method === "" + method &&
+      element[method] instanceof Function
+    ) {
+      method = element[method];
+    }
+    //if it's not a function, use a dummy.
+    else if (!(typeof method === "function")) {
+      method = function() {};
+    }
+    if (method) {
+      myEvents[key] = method.bind(element);
+      events.set(this, myEvents);
+    }
   };
   ep.removeKeyAction = function removeKeyAction(key) {
     var myEvents = events.get(this);
     if (myEvents && myEvents[key]) {
       delete myEvents[key];
-      Object.keys(myEvents).length ? events.set(this,myEvents) : events.delete(this);
+      Object.keys(myEvents).length
+        ? events.set(this, myEvents)
+        : events.delete(this);
     }
   };
-  ep.captureKeyAction = function captureKeyAction(key,method,element) {
-      var myCaptures = captures.get(this) || {};
-      if (!myCapture[key] ) {
-        captureKeys[key] ? captureKeys[key]++ : captureKeys[key] = 1;
-      }
-      element = element instanceof Element ? element : this;
-      if (method && method === ''+method &&  element[method] instanceof Function) {
-        method = element[method];
-      }
-      else if (!(method instanceof Function)) {
-        method = function() {};
-      }
-      myCaptures[key] = method.bind(element);
-      captures.set(this,myCaptures);
-  }
+  ep.captureKeyAction = function captureKeyAction(key, method, element) {
+    var myCaptures = captures.get(this) || {};
+    if (!myCaptures[key]) {
+      captureKeys[key] ? captureKeys[key]++ : (captureKeys[key] = 1);
+    }
+    element = element instanceof Element ? element : this;
+    if (
+      method &&
+      method === "" + method &&
+      element[method] instanceof Function
+    ) {
+      method = element[method];
+    } else if (!(method instanceof Function)) {
+      method = function() {};
+    }
+    myCaptures[key] = method.bind(element);
+    captures.set(this, myCaptures);
+  };
   ep.releaseKeyAction = function releaseKeyEvent(key) {
     var myCaptures = captures.get(this);
     if (myCaptures && myCaptures[this]) {
       delete myCaptures[this];
       captureKeys[key]--;
-      Object.keys(myCaptures).length ? captures.set(this,myCaptures) : captures.delete(this);
+      Object.keys(myCaptures).length
+        ? captures.set(this, myCaptures)
+        : captures.delete(this);
     }
-  }
-  document.addEventListener('keydown',function onDocumentKeydown(e) {
-    //assumes key is polyfilled https://github.com/cvan/keyboardevent-key-polyfill
-    var key = (e.key !== ' ' ? e.key : 'Space'),
+  };
+  document.addEventListener(
+    "keydown",
+    function onDocumentKeydown(e) {
+      //assumes key is polyfilled https://github.com/cvan/keyboardevent-key-polyfill
+      var key = e.key !== " " ? e.key : "Space",
         activeElement = document.activeElement,
         myEvents = events.get(activeElement),
         captureElement = activeElement;
-    //if in content editable mode, ignore
-    if (activeElement.isContentEditable) return;
-    key = (e.altKey && key !== 'Alt' ? 'Alt ' : '') +
-          (e.ctrlKey && key !== 'Control'? 'Control ' : '') +
-          (e.keyMeta && key !== 'Meta'? 'Meta ' : '') +
-          (e.shiftKey && key.length > 1 && key !== 'Shift' ? 'Shift ' : '') + key;
-    if (myEvents && myEvents[key] ) {
-      if (activeElement.dispatchEvent(getKeynavEvent())) {
-        e.preventDefault();
-        myEvents[key]();
-      }
-    }
-    //tab-next set and behaviour not overriden with custom keyAction
-    else if (key == 'Tab' && (activeElement = activeElement.tabNext)) {
-      activeElement.focus();
-      e.preventDefault();
-    }
-    //tab-previous set and behaviour not overriden with custom keyAction
-    else if (key == 'Shift Tab' && (activeElement = activeElement.tabPrevious)) {
-      activeElement.focus();
-      e.preventDefault();
-    }
-    else if (captureKeys[key]){
-      do {
-        captureElement = captureElement.parentElement;
-        myEvents = captures.get(captureElement);
-      } while(captureElement && !myEvents || !myEvents[key]);
-      if (captureElement) {
-        if (captureElement.dispatchEvent(getKeynavEvent())) {
+      //if in content editable mode, ignore
+      if (activeElement.isContentEditable) return;
+      key =
+        (e.altKey && key !== "Alt" ? "Alt " : "") +
+        (e.ctrlKey && key !== "Control" ? "Control " : "") +
+        (e.keyMeta && key !== "Meta" ? "Meta " : "") +
+        (e.shiftKey && key.length > 1 && key !== "Shift" ? "Shift " : "") +
+        key;
+      if (myEvents && myEvents[key]) {
+        if (activeElement.dispatchEvent(getKeynavEvent())) {
           e.preventDefault();
           myEvents[key]();
         }
       }
-    }
-    function getKeynavEvent() {
-      return new CustomEvent('keynavigation', {
-        'cancelable':true,
-        'bubbles':true,
-        details: {
-          'key':e.key,
-          'altKey':e.altKey,
-          'ctrlKey':e.ctrlKey,
-          'shiftKey':e.shiftKey,
-          'metaKey':e.metaKey,
-          'relatedTarget':activeElement
+      //tab-next set and behaviour not overriden with custom keyAction
+      else if (key == "Tab" && (activeElement = activeElement.tabNext)) {
+        activeElement.focus();
+        e.preventDefault();
+      }
+      //tab-previous set and behaviour not overriden with custom keyAction
+      else if (
+        key == "Shift Tab" &&
+        (activeElement = activeElement.tabPrevious)
+      ) {
+        activeElement.focus();
+        e.preventDefault();
+      } else if (captureKeys[key]) {
+        do {
+          captureElement = captureElement.parentElement;
+          myEvents = captures.get(captureElement);
+        } while (captureElement && (!myEvents || !myEvents[key]));
+        if (captureElement && captureElement.dispatchEvent(getKeynavEvent())) {
+          e.preventDefault();
+          myEvents[key]();
         }
-      });
-    }
-  },true);
-}();
+      }
+      function getKeynavEvent() {
+        return new CustomEvent("keynavigation", {
+          cancelable: true,
+          bubbles: true,
+          details: {
+            key: e.key,
+            altKey: e.altKey,
+            ctrlKey: e.ctrlKey,
+            shiftKey: e.shiftKey,
+            metaKey: e.metaKey,
+            relatedTarget: activeElement
+          }
+        });
+      }
+    },
+    true
+  );
+})();
