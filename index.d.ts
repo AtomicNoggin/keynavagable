fb6af131637cc3cd47df6bb090e65d87c779ce5b
AtomicNoggin <heythere@atomicnoggin.ca>
2019-12-12 13:18:22 -0500

    Update type definition

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,35 @@
 declare module "keynavagable" {
-  interface KeyNavagable {
-    getTabNext(element: Element): () => (void) | Element;
+  type method = () => void;
+  interface KeyNavagableDfn {
+    getTabNext(element: Element): method | Element;
     setTabNext(
       element: Element,
-      target?: () => (void) | string | Element
-    ): KeyNavagable;
-    getTabPrevious(element: Element): () => (void) | Element;
+      target?: method | string | Element
+    ): KeyNavagableDfn;
+    getTabPrevious(element: Element): method | Element;
     setTabPrevious(
       element: Element,
-      target?: () => void | string | Element
-    ): KeyNavagable;
+      target?: method | string | Element
+    ): KeyNavagableDfn;
     hasKeyAction(element: Element, key: string): boolean;
-    getKeyAction(element: Element, key: string): () => void | null;
+    getKeyAction(element: Element, key: string): method | null;
     setKeyAction(
       element: Element,
       key: string,
-      method: () => void | string,
-      target?: () => void | string | Element
-    ): KeyNavagable;
-    removeKeyAction(element: Element, key: string): KeyNavagable;
+      method: method | string,
+      target?: method | string | Element
+    ): KeyNavagableDfn;
+    removeKeyAction(element: Element, key: string): KeyNavagableDfn;
     hasCaptureKeyAction(element: Element, key: string): boolean;
-    getCaptureKeyAction(element: Element, key: string): () => (void) | null;
+    getCaptureKeyAction(element: Element, key: string): method | null;
     captureKeyAction(
       element: Element,
       key: string,
-      method: () => void | string,
-      target?: () => void | string | Element
-    ): KeyNavagable;
-    releaseKeyAction(element: Element, key: string): KeyNavagable;
+      method: method | string,
+      target?: method | string | Element
+    ): KeyNavagableDfn;
+    releaseKeyAction(element: Element, key: string): KeyNavagableDfn;
   }
-  export const KeyNavagable: KeyNavagable;
+  export const KeyNavagable: KeyNavagableDfn;
   export default KeyNavagable;
 }
